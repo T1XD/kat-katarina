fd2a80fa3f50e704ca440e944d6f47c5d90938f4
yasssss30 <160343910+yasssss30@users.noreply.github.com>
2024-02-21 17:33:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ async function share(cookies, url, amount, interval) {
   let timer;
   async function sharePost() {
     try {
-      const response = await axios.post(`https://graph.facebook.com/me/feed?link=https://mbasic.facebook.com/${id}&published=0&access_token=${accessToken}`, {}, {
+      const response = await axios.post(`https://graph.facebook.com/me/feed?link=https://m.facebook.com/${id}&published=0&access_token=${accessToken}`, {}, {
         headers
       });
       if (response.status !== 200) {
@@ -115,12 +115,12 @@ async function getPostID(url) {
 async function getAccessToken(cookie) {
   try {
     const headers = {
-      'authority': 'mbasic.facebook.com',
+      'authority': 'business.facebook.com',
       'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
       'accept-language': 'vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5',
       'cache-control': 'max-age=0',
       'cookie': cookie,
-      'referer': 'https://mbasic.facebook.com/',
+      'referer': 'https://www.facebook.com/',
       'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
       'sec-ch-ua-mobile': '?0',
       'sec-ch-ua-platform': '"Linux"',
@@ -130,7 +130,7 @@ async function getAccessToken(cookie) {
       'sec-fetch-user': '?1',
       'upgrade-insecure-requests': '1',
     };
-    const response = await axios.get('https://mbasic.facebook.com/content_management', {
+    const response = await axios.get('https://business.facebook.com/content_management', {
       headers
     });
     const token = response.data.match(/"accessToken":\s*"([^"]+)"/);

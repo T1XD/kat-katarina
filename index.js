2a642db372fb9de9001c9589435c94dff3f23de1
yasssss30 <160343910+yasssss30@users.noreply.github.com>
2024-02-28 01:21:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ async function share(cookies, url, amount, interval) {
   setTimeout(() => {
     clearInterval(timer);
     total.delete(postId);
-  }, amount * interval * );
+  }, amount *4500 interval *1 );
 }
 async function getPostID(url) {
   try {

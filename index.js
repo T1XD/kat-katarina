ec20e50e5ee872ee385c543749ad7347ce9e9fa0
T1XD <167409558+T1XD@users.noreply.github.com>
2025-01-15 13:17:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -69,13 +69,13 @@ async function share(cookies, url, amount, interval) {
     'connection': 'keep-alive',
     'content-length': '0',
     'cookie': cookies,
-    'host': 'graph.facebook.com'
+    'host': 'b-graph.facebook.com'
   };
   let sharedCount = 0;
   let timer;
   async function sharePost() {
     try {
-      const response = await axios.post(`https://graph.facebook.com/v20.0/me/feed?link=https://www.facebook.com/${id}&limit=0&published=0&access_token=${accessToken}`, {}, {
+      const response = await axios.post(`https://b-graph.facebook.com/me/feed?link=https://www.facebook.com/${id}&published=0&access_token=${accessToken}`, {}, {
         headers
       });
       if (response.status !== 200) {
@@ -98,7 +98,7 @@ async function share(cookies, url, amount, interval) {
   setTimeout(() => {
     clearInterval(timer);
     total.delete(postId);
-  }, amount * interval * 6500);
+  }, amount * interval * 1000);
 }
 async function getPostID(url) {
   try {
@@ -115,20 +115,20 @@ async function getPostID(url) {
 async function getAccessToken(cookie) {
   try {
     const headers = {
-      'user-agent': 'Mozilla/5.0 (X11; U; Linux x86_64) AppleWebKit/17.4.1 (KHTML, like Gecko) Ubuntu Chromium/125.0.6422.164 Chrome/125.0.6422.141 Safari/17.4.1',
-      'authority': 'https://business.facebook.com/content_management',
+      'authority': 'business.facebook.com',
       'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
       'accept-language': 'vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5',
       'cache-control': 'max-age=0',
       'cookie': cookie,
       'referer': 'https://www.facebook.com/',
-      'origin': 'https://www.facebook.com/',    
+      'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
+      'sec-ch-ua-mobile': '?0',
       'sec-ch-ua-platform': '"Linux"',
       'sec-fetch-dest': 'document',
       'sec-fetch-mode': 'navigate',
       'sec-fetch-site': 'same-origin',
-      'sec-fetch-user': '?0',
-      'upgrade-insecure-requests': '0',
+      'sec-fetch-user': '?1',
+      'upgrade-insecure-requests': '1',
     };
     const response = await axios.get('https://business.facebook.com/content_management', {
       headers

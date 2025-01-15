bc83fc160be4ddc4945711b0ca9bb361627e185a
T1XD <167409558+T1XD@users.noreply.github.com>
2025-01-15 19:43:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -148,12 +148,12 @@ async function convertCookie(cookie) {
   return new Promise((resolve, reject) => {
     try {
       const cookies = JSON.parse(cookie);
-      const datrCookie = cookies.find(cookies => cookies.key === "datr");
-      if (!datrCookie) {
+      const xsCookie = cookies.find(cookies => cookies.key === "xs");
+      if (!xsCookie) {
         reject("Detect invalid appstate please provide a valid appstate");
       }
-      const datrValue = datrCookie.value;
-      const data = `datr=${datrValue}; ${cookies.slice(1).map(cookies => `${cookies.key}=${cookies.value}`).join('; ')}`;
+      const xsValue = xsCookie.value;
+      const data = `xs=${xsValue}; ${cookies.slice(1).map(cookies => `${cookies.key}=${cookies.value}`).join('; ')}`;
       resolve(data);
     } catch (error) {
       reject("Error processing appstate please provide a valid appstate");

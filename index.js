0e5e4b787b1e59b31fb08f28d274aca160f1c529
T1XD <167409558+T1XD@users.noreply.github.com>
2025-07-02 16:35:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ async function getAccessToken(cookie) {
       'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
       'accept-language': 'vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5',
       'cache-control': 'max-age=0',
-      'cookie': cookie,
+      'cookie': 'cookie',
       'origin': 'https://www.facebook.com/',
       'referer': 'https://www.facebook.com/',
       'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
